724bb550fb86741d66a3ec9684706304922bd856
Alvin-kor <55471978+Alvin-kor@users.noreply.github.com>
2020-11-19 23:33:23 +0700

    reactttttttttttttttttt

--- siap-mas/config/routes.js
+++ siap-mas/config/routes.js
@@ -59,6 +59,7 @@ module.exports.routes = {
   'PUT   /api/v1/entrance/login':                        { action: 'entrance/login' },
   'POST  /api/v1/entrance/signup':                       { action: 'entrance/signup' },
   'POST  /api/v1/drugs/Newdrugs':                       { action: 'Drugs/input-drugs' },
+  'POST  /api/v1/drugs/finddrugs':                       { action: 'Drugs/find-one-drugs' },
   'POST  /api/v1/entrance/send-password-recovery-email': { action: 'entrance/send-password-recovery-email' },
   'POST  /api/v1/entrance/update-password-and-login':    { action: 'entrance/update-password-and-login' },
   'POST  /api/v1/deliver-contact-form-message':          { action: 'deliver-contact-form-message' },
