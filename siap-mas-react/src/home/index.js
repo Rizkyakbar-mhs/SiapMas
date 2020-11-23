83c6118c97e601a1eafb8e7aa0a70088fa33f66a
Alvin <55471978+Alvin-kor@users.noreply.github.com>
2020-11-23 13:23:24 +0900

    background

--- siap-mas-react/src/home/index.js
+++ siap-mas-react/src/home/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import MyImage from '../assets/dog.jpg';
-
+import { motion } from "framer-motion";
  function Index() {
      return(
       <div className="home containt">
-      <h1>
+      <motion.h1 animate={{ fontSize:50 }}>
           Welcome to SiapMas
-      </h1>
-      <img src={MyImage} />
+      </motion.h1>
+      {/* <img src={MyImage} /> */}
       </div>)
  }
 
