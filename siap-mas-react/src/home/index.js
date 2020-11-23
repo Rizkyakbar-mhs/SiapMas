99ff024ea1f2727d9e66d39b97ec024e694aae31
Rizky Akbar <rizkyakbar2000@gmail.com>
2020-11-23 13:32:28 +0700

    background

--- siap-mas-react/src/home/index.js
+++ siap-mas-react/src/home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import MyImage from '../assets/dog.jpg';
+import MyImage from '../assets/LoginBox.png';
 import styles from '../index.css';
 import { motion } from "framer-motion";
  function Index() {
@@ -7,7 +7,7 @@ import { motion } from "framer-motion";
       <div className={styles.container}>
       <motion.h1 animate={{ fontSize:50 }} drag onDrag={ (e) => console.log("x = " + Math.trunc(e.x) + "   " + "y = " + Math.trunc(e.y)) }>
       </motion.h1>
-      {/* <img src={MyImage} /> */}
+       <img src={MyImage} /> 
       </div>)
  }
 
