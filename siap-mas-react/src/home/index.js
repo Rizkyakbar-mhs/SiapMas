8c2a48550ceb78b64eac78a41d1638408e7611dc
Rizky Akbar <rizkyakbar2000@gmail.com>
2020-11-23 16:16:54 +0700

    enter

--- siap-mas-react/src/home/index.js
+++ siap-mas-react/src/home/index.js
@@ -8,9 +8,10 @@ import { motion } from "framer-motion";
       <motion.h1 animate={{ fontSize:50 }} drag onDrag={ (e) => console.log("x = " + Math.trunc(e.x) + "   " + "y = " + Math.trunc(e.y)) }>
       </motion.h1>
        <motion.img src={MyImage} 
-            initial={{ width:"25%",height:"60%",x:1100,y:150,scale:0 }} 
+            initial={{ width:"25%",height:"60%",x:"290%",y:"36%",scale:0 }} 
             transition={{ duration:0.4 }} 
             animate={{ scale:1 }} /> 
+            
       </div>)
  }
 
