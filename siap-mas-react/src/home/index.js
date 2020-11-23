8d8fafb36ff49ff0f2466544eca8ecfceea4cf7d
Alvin <55471978+Alvin-kor@users.noreply.github.com>
2020-11-23 16:06:21 +0900

    Update index.js

--- siap-mas-react/src/home/index.js
+++ siap-mas-react/src/home/index.js
@@ -4,10 +4,13 @@ import styles from '../index.css';
 import { motion } from "framer-motion";
  function Index() {
      return(
-      <div className={styles.container}>
+      <div>
       <motion.h1 animate={{ fontSize:50 }} drag onDrag={ (e) => console.log("x = " + Math.trunc(e.x) + "   " + "y = " + Math.trunc(e.y)) }>
       </motion.h1>
-       <img src={MyImage} /> 
+       <motion.img src={MyImage} 
+            initial={{ width:"25%",height:"60%",x:1100,y:150,scale:0 }} 
+            transition={{ duration:0.4 }} 
+            animate={{ scale:1 }} /> 
       </div>)
  }
 
