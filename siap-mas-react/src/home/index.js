47d461279a53fade3e5c93eabec4b5937d129ee8
Alvin <55471978+Alvin-kor@users.noreply.github.com>
2020-11-23 14:30:35 +0900

    .

--- siap-mas-react/src/home/index.js
+++ siap-mas-react/src/home/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import MyImage from '../assets/dog.jpg';
+import styles from '../index.css';
 import { motion } from "framer-motion";
  function Index() {
      return(
-      <div className="container">
-      <motion.h1 animate={{ fontSize:50 }}>
+      <div className={styles.container}>
+      <motion.h1 animate={{ fontSize:50 }} drag onDrag={ (e) => console.log(e.x,e.y) }>
           Welcome to SiapMas
       </motion.h1>
       {/* <img src={MyImage} /> */}
--- siap-mas-react/src/index.js
+++ siap-mas-react/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Index from "./home/index";
 import MyImage from "./assets/dog.jpg";
-import './index.css';
 import { Route,Link, Switch } from "react-router-dom";
 
 function App () {
