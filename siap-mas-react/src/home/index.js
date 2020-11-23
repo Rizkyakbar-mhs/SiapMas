1d0c9df8c513a6fafa56377f66013d5706dae8a0
Rizky Akbar <rizkyakbar2000@gmail.com>
2020-11-23 10:02:52 +0700

    react not working help

---
+++ siap-mas-react/src/home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+ import {Link} from "react-router-dom" ;
+
+ function index() {
+     return
+      <div className="home containt">
+      <h1>
+          Welcome to SiapMas
+      </h1>
+      <img src="../assets/dog.jpg" />
+      </div>
+ }
+
+ export default index ;
