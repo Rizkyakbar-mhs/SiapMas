848e2c47e53e09b26a359abe844685874ba23b1d
adm <ryzaldm@gmail.com>
2020-11-26 09:55:46 +0700

    User

---
+++ siap-mas/api/models/User.js
@@ -0,0 +1,49 @@
+module.exports = {
+
+  attributes: {
+
+    NIP: {
+      type: 'string',
+      required: true,
+      unique: true,
+    },
+
+    Name: {
+      type: 'string',
+      required: true,
+      maxLength: 200,
+      example: 'Alvin'
+    },
+
+    Username: {
+      type: 'string',
+      required: true,
+    },
+
+    Password: {
+      type: 'string',
+      required: true,
+    },
+    
+    BirthPlace: {
+      type: 'string',
+      required: true,
+      maxLength: 50
+    },
+
+    BirthDate: {
+      type: 'date',
+      required: true,
+      maxLength: 50
+    },
+    
+    Address: {
+      type: 'string',
+      required: true,
+      maxLength: 200
+    },
+
+  },
+
+};
+
