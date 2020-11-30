af08ac491957ad5ea981528a88238f748ac90cbc
Rizky Akbar <rizkyakbar2000@gmail.com>
2020-11-30 10:17:45 +0700

    polygon

--- siap-mas-react/src/dashboard/Dashboard.js
+++ siap-mas-react/src/dashboard/Dashboard.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Polygon from '../assets/Base_Icon.png';
+import { motion } from "framer-motion";
+import './index.css';
 function Dash () {
 return(
-        <div>
+    
+    <div>
+        <img src={Polygon} 
+            className="img"
             
-        </div>
+        />
+    </div>    
+        
+            
+        
 )
 }
 
