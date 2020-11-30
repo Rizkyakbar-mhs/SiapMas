8f9bc5c5a8b3dc375455ffdfdd3cb4766876dc2e
Alvin <55471978+Alvin-kor@users.noreply.github.com>
2020-11-30 11:46:36 +0900

    .

--- siap-mas-react/src/dashboard/Dashboard.js
+++ siap-mas-react/src/dashboard/Dashboard.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Polygon from './Polygon';
 function Dash () {
 return(
-    <div>
-        <Polygon />
-        <div style={{ marginRight:"30%" }}>
-        <Polygon />
+        <div>
+            
         </div>
-    </div>
 )
 }
 
