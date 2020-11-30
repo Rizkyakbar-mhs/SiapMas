59d49815a6b2957b8bae323a639af84989046e1d
Rizky Akbar <rizkyakbar2000@gmail.com>
2020-11-30 12:21:33 +0700

    polygon design

--- siap-mas-react/src/dashboard/Dashboard.js
+++ siap-mas-react/src/dashboard/Dashboard.js
@@ -4,16 +4,37 @@ import './index.css';
 function Dash () {
 return(
 <div>
-
-        <div className="Polygon-Warp">
+        
+        <div className="Polygon-Warp ">
             <div className="Polygon">
             </div>
         </div>
-    
+
+     
         <div className="Polygon-Warp Polygon-1">
             <div className="Polygon Polygon-Warp Polygon-1">
         </div>
     </div>
+
+    <div className="Polygon-Warp Polygon-2 ">
+            <div className="Polygon">
+            </div>
+        </div>
+
+        <div className="Polygon-Warp Polygon-3 ">
+            <div className="Polygon">
+            </div>
+        </div>
+        <div className="Polygon-Warp Polygon-4 ">
+            <div className="Polygon">
+            </div>
+        </div>
+        <div className="Polygon-Warp Polygon-5 ">
+            <div className="Polygon">
+            </div>
+        </div>
+
+        
 </div>
 )
 }
