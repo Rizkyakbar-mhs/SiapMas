038ac59322542a574f95994aa2074a18e56e961d
Rizky Akbar <rizkyakbar2000@gmail.com>
2020-12-07 12:04:57 +0700

    Update input_regis.js

--- siap-mas-react/src/Input/input_regis.js
+++ siap-mas-react/src/Input/input_regis.js
@@ -22,57 +22,86 @@ return(
      <div className="container-form">
 
      <div className="div1">
-     <input 
+     <motion.input 
                     type="text" 
                     placeholder="Masukkan Nip" 
                     id="Nip"
-                    className="InputNip"/>
+                    className="InputNip"
+                    transition={{delay:0.2,duration:1}}
+                    initial={{scale:0}}
+                        animate={{scale:1}}
+                    />
                     </div>
                     <div className="div2    ">
-                    <input 
+                    <motion.input 
                     type="text" 
                     placeholder="Masukkan Password" 
                     id="Password"
-                    className="InputPasswords"/>
+                    className="InputPasswords"
+                    transition={{delay:0.7,duration:1}}
+                    initial={{scale:0}}
+                        animate={{scale:1}}
+                    />
                     </div>
                     <div  className="div4">
-                    <input 
+                    <motion.input 
                     type="text" 
                     placeholder="Konfirmasi Password" 
                     id="Konfirmasi"
-                    className="InputKonfirm"/>
+                    className="InputKonfirm"
+                    transition={{delay:0.8,duration:1}}
+                    initial={{scale:0}}
+                        animate={{scale:1}}
+                    />
                     </div>
                     
                     <div  className="div3">
-                    <input 
+                    <motion.input 
                     type="text" 
                     placeholder="Masukkan Nama" 
                     id="Nama"
-                    className="InputNama"/>
+                    className="InputNama"
+                    transition={{delay:0.3,duration:1}}
+                    initial={{scale:0}}
+                        animate={{scale:1}}
+                    />
                     </div>
                     <div className="div1">
-                    <input 
+                    <motion.input 
                     type="text" 
                     placeholder="Masukkan Email" 
                     id="Email"
-                    className="InputEmail"/>
+                    className="InputEmail"
+                    transition={{delay:0.4,duration:1}}
+                    initial={{scale:0}}
+                        animate={{scale:1}}
+                    />
                     </div>
                     <div className="div1">
-                    <input 
+                    <motion.input 
                     type="text" 
                     placeholder="Masukkan Alamat" 
                     id="Alamat"
-                    className="InputAlamat"/>
+                    className="InputAlamat"
+                    transition={{delay:0.5,duration:1}}
+                    initial={{scale:0}}
+                        animate={{scale:1}}
+                    />
                     </div>
                     <div className="div1">
-                    <input 
+                    <motion.input 
                     type="text" 
                     placeholder="Masukkan Nomer Handphone" 
                     id="Nomer_hp"
-                    className="InputHp"/>
+                    className="InputHp"
+                    transition={{delay:0.6,duration:0.5}}
+                        initial={{scale:0}}
+                        animate={{scale:1}}
+                    />
                     </div>
                     <div className="div5">
                     <motion.input type="submit" 
+                    transition={{delay:0.9,duration:0.5}}
                     initial={{scale:0}}
                     animate={{scale:1}}
                     whileTap={{opacity:"0"}}
