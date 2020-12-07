e734d813fa62de4863ee3be86ac9b9bc65f5dc37
Rizky Akbar <rizkyakbar2000@gmail.com>
2020-12-07 11:08:37 +0700

    Register Form

--- siap-mas-react/src/Input/input_regis.js
+++ siap-mas-react/src/Input/input_regis.js
@@ -21,8 +21,59 @@ return(
         
      <div className="container-form">
 
+     <div className="div1">
+     <input 
+                    type="text" 
+                    placeholder="Masukkan Nip" 
+                    id="Nip"
+                    className="InputNip"/>
+                    </div>
+                    <div className="div2    ">
+                    <input 
+                    type="text" 
+                    placeholder="Masukkan Password" 
+                    id="Nip"
+                    className="InputPasswords"/>
+                    </div>
+                    <div  className="div4">
+                    <input 
+                    type="text" 
+                    placeholder="Konfirmasi Password" 
+                    id="Nip"
+                    className="InputKonfirm"/>
+                    </div>
+
+                    <div  className="div3">
+                    <input 
+                    type="text" 
+                    placeholder="Masukkan Nama" 
+                    id="Nip"
+                    className="InputNama"/>
+                    </div>
+                    <div className="div1">
+                    <input 
+                    type="text" 
+                    placeholder="Masukkan Email" 
+                    id="Nip"
+                    className="InputEmail"/>
+                    </div>
+                    <div className="div1">
+                    <input 
+                    type="text" 
+                    placeholder="Masukkan Alamat" 
+                    id="Nip"
+                    className="InputAlamat"/>
+                    </div>
+                    <div className="div1">
+                    <input 
+                    type="text" 
+                    placeholder="Masukkan Nomer Handphone" 
+                    id="Nip"
+                    className="InputHp"/>
+                    </div>
+                    
      </div>
-       
+     
 
    
 
