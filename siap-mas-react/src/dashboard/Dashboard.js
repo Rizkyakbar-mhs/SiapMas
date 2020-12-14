d98f28150c3bfbfbb3d4fe37c642de72650a137a
adm <ryzaldm@gmail.com>
2020-12-14 14:38:43 +0700

    Update Dashboard.js

--- siap-mas-react/src/dashboard/Dashboard.js
+++ siap-mas-react/src/dashboard/Dashboard.js
@@ -3,48 +3,70 @@ import { motion } from "framer-motion";
 import './index.css';
 import SignOut from '../store/actions/SignOut'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
+
+    const mapStateToProps = (state) => {
+        console.log(state); 
+            return{
+                auth:state.firebase.auth,
+                profile:state.firebase.profile
+            }
+        }
 
     const Dash = (props) => {
         return(
-            <div>
-        <div>
-                <div className="Polygon-Warp Polygon-0 ">
-                    <div className="Polygon">
-                    <div className="Polygon-2-Img"></div>
-                    </div>
-                </div>
+            <motion.div>
+        <motion.div>
+                <motion.div className="Polygon-Warp Polygon-0 ">
+                    <motion.div className="Polygon">
+                    <Link to="/profile">
+                    <motion.div whileHover={{ scale:0.96, transition: { duration: 0.25 } }} className="Polygon-2-Img"></motion.div>
+                    </Link>
+                    </motion.div>
+                </motion.div>
              
-                <div className="Polygon-Warp Polygon-1">
-                    <div className="Polygon Polygon-1">
-                    <div className="Polygon-1-Img"></div>
-                </div>
-            </div>
+                <motion.div className="Polygon-Warp Polygon-1">
+                    <motion.div className="Polygon Polygon-1">
+                    <Link to="/profile">
+                    <motion.div whileHover={{ scale:0.96, transition: { duration: 0.25 } }} className="Polygon-1-Img"></motion.div>
+                    </Link>
+                </motion.div>
+            </motion.div>
         
-            <div className="Polygon-Warp Polygon-2 ">
-                    <div className="Polygon">
-                    <div role="Button" onClick={props.SignOut} className="Polygon-6-Img"></div>
-                    </div>
-                </div>
+            <motion.div className="Polygon-Warp Polygon-2 ">
+                    <motion.div className="Polygon">
+                    <Link to="/profile">
+                    <motion.div whileHover={{ scale:0.96, transition: { duration: 0.25 } }}
+                     className="Polygon-5-Img"></motion.div>
+                     </Link>
+                    </motion.div>
+                </motion.div>
         
-                <div className="Polygon-Warp Polygon-3 ">
-                    <div className="Polygon">
-                    <div className="Polygon-5-Img"></div>
-                    </div>
-                </div>
-                <div className="Polygon-Warp Polygon-4 ">
-                    <div className="Polygon">
-                    <div className="Polygon-4-Img"></div>
-                    </div>
-                </div>
-                <div className="Polygon-Warp Polygon-5 ">
-                    <div className="Polygon">
-                    <div  className="Polygon-3-Img">
-                    </div>
-                    </div>
-                </div>
+                <motion.div className="Polygon-Warp Polygon-3 ">
+                    <motion.div className="Polygon">
+                    <motion.div whileHover={{ scale:0.96, transition: { duration: 0.25 } }} 
+                    role="Button" onClick={props.SignOut} className="Polygon-6-Img"></motion.div>
+                    </motion.div>
+                </motion.div>
+
+                <motion.div className="Polygon-Warp Polygon-4 ">
+                    <motion.div className="Polygon">
+                    <Link to="/profile">
+                    <motion.div whileHover={{ scale:0.96, transition: { duration: 0.25 } }} className="Polygon-4-Img"></motion.div>
+                    </Link>
+                    </motion.div>
+                </motion.div>
+
+                <motion.div className="Polygon-Warp Polygon-5 ">
+                    <motion.div className="Polygon">
+                    <Link to="/profile">
+                    <motion.div whileHover={{ scale:0.96, transition: { duration: 0.25 } }} className="Polygon-3-Img"></motion.div>
+                    </Link> 
+                    </motion.div>
+                </motion.div>
                 
-        </div>
-        </div>
+        </motion.div>
+        </motion.div>
         )
     }
         
