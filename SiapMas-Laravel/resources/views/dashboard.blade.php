4df12a953453aee77ca4627a38006f3cd19be2ee
Alvin <55471978+Alvin-kor@users.noreply.github.com>
2021-01-28 16:27:26 +0900

    .

---
+++ SiapMas-Laravel/resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>SiapMas - Dashboard</title>
+    @include('Templates/Header')
+</head>
+<body>
+    @include('Templates/Navbar')
+    @include('Templates/Footer')
+</body>
+</html>
--- SiapMas-Laravel/resources/views/welcome.blade.php
+++ SiapMas-Laravel/resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-
+    <title>SiapMas - Welcome</title>
         @include('Templates/Header')
 
     </head>
@@ -11,17 +11,6 @@
         @include('Main/Index')
         @include('Templates/Footer')
 
-        <!--   Core JS Files   -->
-  <script src="js/core/jquery.min.js" type="text/javascript"></script>
-  <script src="js/core/popper.min.js" type="text/javascript"></script>
-  <script src="js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
-  <script src="js/plugins/moment.min.js"></script>
-  <!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
-  <script src="js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
-  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-  <script src="js/plugins/nouislider.min.js" type="text/javascript"></script>
-  <!--  Google Maps Plugin    -->
-  <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
-  <script src="js/material-kit.js?v=2.0.7" type="text/javascript"></script>
+        
     </body>
 </html>
