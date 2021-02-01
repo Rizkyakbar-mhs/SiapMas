c684752c38e1e6b8be006c969a75931447d581a6
Rizky Akbar <rizkyakbar2000@gmail.com>
2021-02-01 13:28:04 +0700

    Update index.blade.php

--- SiapMas-Laravel/resources/views/Dashboard/index.blade.php
+++ SiapMas-Laravel/resources/views/Dashboard/index.blade.php
@@ -1,112 +1,81 @@
-<div class="page-header header-filter clear-filter purple-filter" data-parallax="true"
-    style="background-image:url({{ asset('img/bg2.jpg') }})">
+<div class="page-header header-filter clear-filter purple-filter" data-parallax="true" style="background-image:url({{ asset('img/bg2.jpg') }})">
     <div class="container">
-        <div class="row">
-            <div class="col-md-8 ml-auto mr-auto">
-                <div class="brand">
-                    <h1>Material Kit.</h1>
-                    <h3>A Badass Bootstrap 4 UI Kit based on Material Design.</h3>
-                </div>
-            </div>
+      <div class="row">
+        <div class="col-md-8 ml-auto mr-auto">
+          <div class="brand">
+            <h1>Welcome to Sistem Informasi Apotek Puskesmas Sumber Wringin</h1>
+            <h3>jalan wringin no 4</h3>
+          </div>
         </div>
-    </div>
+      </div>
+    </div>  
 </div>
 <div class="main main-raised">
     <div class="section sction-basic">
         <div class="container">
-            <!--         carousel  -->
-            <div class="section" id="carousel">
-                <div class="container">
+                        <!--         carousel  -->
+                <div class="section" id="carousel">
+                    <div class="container">
                     <div class="row">
                         <div class="col-md-8 mr-auto ml-auto">
-                            <!-- Carousel Card -->
-                            <div class="card card-raised card-carousel">
-                                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel"
-                                    data-interval="3000">
-                                    <ol class="carousel-indicators">
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">
-                                        </li>
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                                    </ol>
-                                    <div class="carousel-inner">
-                                        <div class="carousel-item active">
-                                            <img class="d-block w-100" src="img/bg2.jpg" alt="First slide">
-                                            <div class="carousel-caption d-none d-md-block">
-                                                <h4>
-                                                    <i class="material-icons">location_on</i>
-                                                    Yellowstone National Park, United States
-                                                </h4>
-                                            </div>
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img class="d-block w-100" src="img/bg3.jpg" alt="Second slide">
-                                            <div class="carousel-caption d-none d-md-block">
-                                                <h4>
-                                                    <i class="material-icons">location_on</i>
-                                                    Somewhere Beyond, United States
-                                                </h4>
-                                            </div>
-                                        </div>
-                                        <div class="carousel-item">
-                                            <img class="d-block w-100" src="img/bg.jpg" alt="Third slide">
-                                            <div class="carousel-caption d-none d-md-block">
-                                                <h4>
-                                                    <i class="material-icons">location_on</i>
-                                                    Yellowstone National Park, United States
-                                                </h4>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button"
-                                        data-slide="prev">
-                                        <i class="material-icons">keyboard_arrow_left</i>
-                                        <span class="sr-only">Previous</span>
-                                    </a>
-                                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button"
-                                        data-slide="next">
-                                        <i class="material-icons">keyboard_arrow_right</i>
-                                        <span class="sr-only">Next</span>
-                                    </a>
+                        <!-- Carousel Card -->
+                        <div class="card card-raised card-carousel">
+                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="3000">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                <img class="d-block w-100" src="img/bg2.jpg" alt="First slide">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h4>
+                                    <i class="material-icons">location_on</i>
+                                    Yellowstone National Park, United States
+                                    </h4>
+                                </div>
+                                </div>
+                                <div class="carousel-item">
+                                <img class="d-block w-100" src="img/bg3.jpg" alt="Second slide">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h4>
+                                    <i class="material-icons">location_on</i>
+                                    Somewhere Beyond, United States
+                                    </h4>
+                                </div>
+                                </div>
+                                <div class="carousel-item">
+                                <img class="d-block w-100" src="img/bg.jpg" alt="Third slide">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h4>
+                                    <i class="material-icons">location_on</i>
+                                    Yellowstone National Park, United States
+                                    </h4>
+                                </div>
                                 </div>
                             </div>
-                            <!-- End Carousel Card -->
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <i class="material-icons">keyboard_arrow_left</i>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <i class="material-icons">keyboard_arrow_right</i>
+                                <span class="sr-only">Next</span>
+                            </a>
+                            </div>
+                        </div>
+                        <!-- End Carousel Card -->
                         </div>
                     </div>
+                    </div>
                 </div>
-            </div>
-            <!--         end carousel -->
-            <div class="row">
-                <div style="text-align:center;color:red" class="col-md-4">
-                    Barang Telah Kadaluarsa
+                <!--         end carousel -->
+                <div class="progress" style="height:10px;border-radius:25px">
+                    <div class="progress-bar bg-danger" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress-bar bg-warning" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <div style="text-align:center;color:orange" class="col-md-4">
-                    Barang Mendekati Kadaluarsa
-                </div>
-                <div style="text-align:center;color:green" class="col-md-4">
-                    Barang Baru
-                </div>
-            </div>
-            <div class="progress" style="height:10px;border-radius:25px">
-                <div class="progress-bar bg-danger" role="progressbar" style="width: 30%" aria-valuenow="10"
-                    aria-valuemin="0" aria-valuemax="100">
-                    <button type="button" class="btn btn-danger" data-placement="bottom" data-toggle="tooltip"
-                        data-html="true" title="<em>Tooltip</em> <u>with</u> <b>HTML</b>">
-                    </button>
-                </div>
-                <div class="progress-bar bg-warning" role="progressbar" style="width: 40%" aria-valuenow="40"
-                    aria-valuemin="0" aria-valuemax="100">
-                    <button type="button" class="btn btn-warning" data-placement="bottom" data-toggle="tooltip"
-                        data-html="true" title="<em>Tooltip</em> <u>with</u> <b>HTML</b>">
-                    </button>
-                </div>
-                <div class="progress-bar bg-success" role="progressbar" style="width: 30%" aria-valuenow="50"
-                    aria-valuemin="0" aria-valuemax="100">
-                    <button type="button" class="btn btn-success" data-placement="bottom" data-toggle="tooltip"
-                        data-html="true" title="<em>Tooltip</em> <u>with</u> <b>HTML</b>">
-                    </button>
-                </div>
-            </div>
         </div>
     </div>
 </div>
