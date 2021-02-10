0213ac66004b9d4cf8759ff20a8b612e5dbb6851
Rizky Akbar <rizkyakbar2000@gmail.com>
2021-02-10 16:30:06 +0700

    Update index.blade.php

--- SiapMas-Laravel/resources/views/Profile/index.blade.php
+++ SiapMas-Laravel/resources/views/Profile/index.blade.php
@@ -27,22 +27,22 @@
                             <div class="form-row">
                                 <div class="form-group col-md-6">
                                     <label for="NIP" class="bmd-label-floating">Kode Obat...</label>
-                                    <input type="text" class="form-control" id="kd_obat">
+                                   
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label for="Nama" class="bmd-label-floating">Nama Obat...</label>
-                                    <input type="text" class="form-control" id="Nama_obat">
+                                   
                                 </div>
                             </div>
 
                             <div class="form-row">
                                 <div class="form-group col-md-6">
                                     <label for="Email" class="bmd-label-floating">Stock...</label>
-                                    <input type="text" class="form-control" id="Stock">
+                                   
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label for="HP" class="bmd-label-floating">Harga...</label>
-                                    <input type="text" class="form-control" id="HP">
+                                    
                                 </div>
                             </div>
 
@@ -50,7 +50,7 @@
 
                             <div class="form-group">
                                 <label id="forexp" class="label-control">Tanggal Kadaluarsa</label>
-                                <input type="text" class="form-control datetimepicker" id="exp">
+                                
                             </div>
                         </div>
                     </div>
